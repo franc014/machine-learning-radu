3b07e97900d36c99be79731d3df25374872c5987
Francisco Andrade <jfandradea@gmail.com>
2024-04-24 15:43:54 -0500

    display scketches in visualizer page

--- src/common/constants.js
+++ src/common/constants.js
@@ -1,10 +1,12 @@
 const constants = {};
 
-constants.DATA_DIR = "../data/";
+constants.PUBLIC_DIR = "../../public";
+console.log(constants.PUBLIC_DIR);
+constants.DATA_DIR = "../data";
 constants.RAW_DIR = constants.DATA_DIR + "/raw";
 constants.DATASET_DIR = constants.DATA_DIR + "/dataset";
 constants.JSON_DIR = constants.DATASET_DIR + "/json";
-constants.IMG_DIR = constants.DATASET_DIR + "/img";
+constants.IMG_DIR = constants.PUBLIC_DIR + "/data/img";
 
 constants.SAMPLES = constants.DATASET_DIR + "/samples.json";
 constants.JS_OBJECTS = "../common/js_objects";
--- src/common/utils.js
+++ src/common/utils.js
@@ -7,4 +7,26 @@ utils.printProgress = (count, max) => {
   process.stdout.write(`${count}/${max} (${percent})`);
 };
 
+utils.groupBy = (key, samples) => {
+  function groupByReducer(acc, curr, i) {
+    if (!acc[curr[key]]) {
+      acc[curr[key]] = [
+        {
+          drawing: curr.label,
+          img: `${i + 1}.png`,
+        },
+      ];
+    } else {
+      acc[curr[key]].push({
+        drawing: curr.label,
+        img: `${i + 1}.png`,
+      });
+    }
+
+    return acc;
+  }
+
+  return samples.reduce(groupByReducer, {});
+};
+
 export default utils;
