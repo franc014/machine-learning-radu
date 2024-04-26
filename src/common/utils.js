96fee427c22d1841d0b6a05a04e6e95a2dfa6043
Francisco Andrade <jfandradea@gmail.com>
2024-04-26 13:27:23 -0500

    create promises to improve data handling before showing on the visualizations

--- src/common/utils.js
+++ src/common/utils.js
@@ -7,41 +7,53 @@ utils.printProgress = (count, max) => {
   process.stdout.write(`${count}/${max} (${percent})`);
 };
 
-utils.groupBy = (key, samples) => {
-  function groupByReducer(acc, curr, i) {
-    if (!acc[curr[key]]) {
-      acc[curr[key]] = {
-        student: curr.student_name,
-        drawings: [
-          {
-            drawing: curr.label,
-            img: `${i + 1}.png`,
-            id: i + 1,
-          },
-        ],
-      };
-    } else {
-      acc[curr[key]].drawings.push({
-        drawing: curr.label,
-        img: `${i + 1}.png`,
-        id: i + 1,
-      });
+utils.groupByV2 = (key, samples) => {
+  return new Promise(function (resolve, reject) {
+    function groupByReducer(acc, curr, i) {
+      if (!acc[curr[key]]) {
+        acc[curr[key]] = {
+          student: curr.student_name,
+          drawings: [
+            {
+              drawing: curr.label,
+              img: `${i + 1}.png`,
+              id: i + 1,
+            },
+          ],
+        };
+      } else {
+        acc[curr[key]].drawings.push({
+          drawing: curr.label,
+          img: `${i + 1}.png`,
+          id: i + 1,
+        });
+      }
+
+      return acc;
     }
-
-    return acc;
-  }
-
-  return samples.reduce(groupByReducer, {});
+    try {
+      const groupedBy = Object.values(samples).reduce(groupByReducer, {});
+      resolve(groupedBy);
+    } catch (error) {
+      reject(error);
+    }
+  });
 };
 
-utils.byStudent = (studentsData) => {
-  let byStudent = [];
+utils.byStudentV2 = (studentsData) => {
+  return new Promise(function (resolve, reject) {
+    try {
+      let byStudent = [];
 
-  Object.values(studentsData).forEach((student) => {
-    byStudent.push(student);
+      Object.values(studentsData).forEach((student) => {
+        byStudent.push(student);
+      });
+
+      resolve(byStudent);
+    } catch (error) {
+      reject(error);
+    }
   });
-
-  return byStudent;
 };
 
 utils.styles = {
